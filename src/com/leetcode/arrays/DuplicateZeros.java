35d6a753147e0cc5ff1b2f60dc3b825af1ce2ced
Jilani <sjilani2014@gmail.com>
2020-09-06 17:41:58 +0530

    leetcode - Arrays Duplicate Zeroes P4

--- src/com/leetcode/arrays/DuplicateZeros.java
+++ src/com/leetcode/arrays/DuplicateZeros.java
@@ -4,7 +4,8 @@ public class DuplicateZeros {
 
 	public static void main(String[] args) {
 
-		int[] arr = { 1, 0, 2, 3, 0, 4, 5, 0 };
+		//int[] arr = { 1, 0, 2, 0, 0, 0, 0, 0 };
+		int[] arr= {8,4,5,0,0,1,0,7};
 		int n = arr.length;
 
 		Utils.printArray(arr, n, "Original Array");
@@ -14,6 +15,47 @@ public class DuplicateZeros {
 	}
 
 	static void duplicateZeros(int[] arr) {
+
+		int n = arr.length;
+
+		int pos_dup = 0;
+
+		for (int i = 0; i < n - 1; i++) {
+
+			if (arr[i] == 0) {
+				if (pos_dup + 1 + i < n)
+					pos_dup++;
+				else
+					break;
+			}
+		}
+
+		// Now we have the pos_duplicates.
+
+		int k = n - pos_dup - 1;
+		int j = n - 1;
+		
+		if ( arr[k]==0 ) {
+			if ( k + 1 + pos_dup >= n) {
+				arr[j]= arr[k];
+				j--;
+				k--;
+			}
+		}
+
+		while (k >= 0) {
+			if (arr[k] == 0) {
+				arr[j] = 0;
+				j--;
+			}
+			arr[j] = arr[k];
+			j--;
+			k--;
+		}
+
+	}
+
+	static void duplicateZeros2(int[] arr) {
 		int n = arr.length;
 
 		for (int i = 0; i < n - 1; i++) {
